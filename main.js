d3a8c578ad2a861018b5ed73f5222196c59310a5
Imran Syed <imran@Imrans-MacBook-Air.local>
2022-04-27 13:51:44 -0500

    new

--- main.js
+++ main.js
@@ -4,6 +4,11 @@ $('#toggle-button').click(function(){
     $('nav ul').toggle('slow')
 })
 
+$('.projects .project-items .project1 a img').hover(function(){
+    $( this ).fadeOut( 100 );
+    $( this ).fadeIn(100);
+})
+
 
 
 
