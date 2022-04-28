65ab88636630dcf573d0abf1c2d10c15c95d4b6d
Imran Syed <imran@Imrans-MacBook-Air.local>
2022-04-28 16:40:21 -0500

    api

--- main.js
+++ main.js
@@ -1,8 +1,7 @@
-
 // toggle button for hamburger
-$('#toggle-button').click(function(){
-    $('nav ul').toggle('slow')
-})
+$("#toggle-button").click(function () {
+  $("nav ul").toggle("slow");
+});
 
 // $('.projects .project-items .project1 a img').hover(function(){
 //     $( this ).fadeOut( 100 );
@@ -10,5 +9,18 @@ $('#toggle-button').click(function(){
 // })
 
 
-
-
+function sendEmail() {
+    Email.send({
+        Host : "smtp.yourisp.com",
+        Username : "username",
+        Password : "password",
+        To : 'imran.syed.h3@gmail.com',
+        From : $('#email').val,
+        Subject : "New Contact from Portfolio",
+        Body : "Name: " + $('#name').val
+                + "<br> Email: " + $('#email').val
+                + "<br> Message: " + $('#message').val
+    }).then(
+      message => alert("Message Sent! Will reach back to you shortly!") 
+    );
+}
