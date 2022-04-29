60d4353c847352880e72845c20e90753eeac0244
Imran Syed <imran@Imrans-MacBook-Air.local>
2022-04-28 19:25:47 -0500

    skills

--- main.js
+++ main.js
@@ -1,13 +1,6 @@
-// toggle button for hamburger
 $("#toggle-button").click(function () {
   $("nav ul").toggle("slow");
 });
-
-// $('.projects .project-items .project1 a img').hover(function(){
-//     $( this ).fadeOut( 100 );
-//     $( this ).fadeIn(100);
-// })
-
 
 function sendEmail() {
     Email.send({
