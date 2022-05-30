46b161255dc32553f80606e10bab5d68732aecd1
Imran Syed <imran@Imrans-MacBook-Air.local>
2022-05-29 23:57:37 -0500

    project update

--- main.js
+++ main.js
@@ -2,16 +2,13 @@ $("#toggle-button").click(function () {
   $("nav ul").toggle("slow");
 });
 
-
-var form = document.getElementById('sheetdb-form');
-form.addEventListener("submit", e => {
-    e.preventDefault();
-    fetch(form.action, {
-        method: 'POST',
-        body: new FormData(document.getElementById('sheetdb-form')),
-    }).then(
-        response => response.json()
-    ).then(
-        // you can put any JS code here
-        alert('Info Sent! Will reply shortly')
-)})
+var form = document.getElementById("sheetdb-form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(form.action, {
+    method: "POST",
+    body: new FormData(document.getElementById("sheetdb-form")),
+  })
+    .then((response) => response.json())
+    .then(alert("Info Sent! Will reply shortly"));
+});
